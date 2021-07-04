fb5e1ad49a0e9a5aeb1420ec3e343dbb72a568a9
Vian <praviandyg@gmail.com>
2021-07-04 22:33:42 +0700

    updated settings_docker.py

--- config/settings_docker.py
+++ config/settings_docker.py
@@ -156,6 +156,9 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 # STATICFILES_DIRS = [str(BASE_DIR / "static")]
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
