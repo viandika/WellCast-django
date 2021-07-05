6cb12fe478d46d71dde7f11666bec5aae4b47c9d
Vian <praviandyg@gmail.com>
2021-07-05 22:25:32 +0700

    fix refresh heatmap and added title

--- las_viewer/views.py
+++ las_viewer/views.py
@@ -37,6 +37,64 @@ def one_base_page(request):
         context["las_files"] = request.session["las_list"]
 
     if "columns" in request.session:
+        train_df = pd.read_json(request.session["train_df"])
+        cor = train_df.corr()
+        fig = go.Figure(
+            go.Heatmap(
+                x=cor.index,
+                y=cor.columns,
+                z=cor,
+                hovertemplate="<b>%{x}:%{y}</b> = %{z}<extra></extra>",
+                colorscale=[
+                    # Let first 10% (0.1) of the values have color rgb(0, 0, 0)
+                    [0, "rgb(0, 0, 0)"],
+                    [0.1, "rgb(0, 0, 0)"],
+                    # Let values between 10-20% of the min and max of z
+                    # have color rgb(20, 20, 20)
+                    [0.1, "rgb(20, 20, 20)"],
+                    [0.2, "rgb(20, 20, 20)"],
+                    # Values between 20-30% of the min and max of z
+                    # have color rgb(40, 40, 40)
+                    [0.2, "rgb(40, 40, 40)"],
+                    [0.3, "rgb(40, 40, 40)"],
+                    [0.3, "rgb(60, 60, 60)"],
+                    [0.4, "rgb(60, 60, 60)"],
+                    [0.4, "rgb(80, 80, 80)"],
+                    [0.5, "rgb(80, 80, 80)"],
+                    [0.5, "rgb(100, 100, 100)"],
+                    [0.6, "rgb(100, 100, 100)"],
+                    [0.6, "rgb(120, 120, 120)"],
+                    [0.7, "rgb(120, 120, 120)"],
+                    [0.7, "rgb(140, 140, 140)"],
+                    [0.8, "rgb(140, 140, 140)"],
+                    [0.8, "rgb(160, 160, 160)"],
+                    [0.9, "rgb(160, 160, 160)"],
+                    [0.9, "rgb(180, 180, 180)"],
+                    [0.95, "rgb(180, 180, 180)"],
+                    [0.95, "rgb(230, 230, 230)"],
+                    [1.0, "rgb(230, 230, 230)"],
+                ],
+            )
+        )
+        fig.update_layout(
+            height=300,
+            margin=dict(l=10, r=10, b=10, t=10),
+            title={"text": "Correlation Heat map", "y": 0, "x": 0.5},
+        )
+        config = {
+            "displaylogo": False,
+            "modeBarButtonsToRemove": [
+                "select2d",
+                "lasso2d",
+                "toggleSpikelines",
+                "autoScale2d",
+            ],
+        }
+
+        heatmap_div = fig.to_html(
+            full_html=False, config=config, include_plotlyjs=False
+        )
+        context["heatmap_div"] = heatmap_div
         context["columns"] = request.session["columns"]
 
     if "features" in request.session:
