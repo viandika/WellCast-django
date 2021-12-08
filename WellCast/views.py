2c72b12b166b41fb12ebefef15872bdd756e66a2
Vian <praviandyg@gmail.com>
2021-12-08 14:29:02 +0700

    rename config folder

--- WellCast/views.py
+++ WellCast/views.py
@@ -1,7 +1,10 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from wellcast.forms import LasUploadForm
+from wellcast.models import LasUpload
 
 
 def welcome_page(request):
@@ -12,10 +15,30 @@ def welcome_page(request):
 def upload_files(request):
     if request.method == "POST":
         form = LasUploadForm(request.POST, request.FILES)
+        uploaded_file_ids = []
         if form.is_valid():
             for file in request.FILES.getlist("las_file"):
-                print(file.name)
-        return HttpResponse('done')
+                instance = LasUpload(
+                    las_file=file,
+                    filename=file.name,
+                )
+                instance.save()  # save form to database
+                uploaded_file_ids.append(instance.pk.hashid)
+            file_names_qs = LasUpload.objects.filter(pk__in=uploaded_file_ids).values(
+                "las_file"
+            )
+            file_names = [os.path.basename(file["las_file"]) for file in file_names_qs]
+            # add file names to sesion
+            if "las_list" in request.session:
+                las_list = request.session["las_list"]
+                las_list.extend(file_names)
+                request.session["las_list"] = las_list
+            else:
+                las_list = file_names
+                request.session["las_list"] = file_names
+
+            context = {"las_files": las_list}
+            return render(request, "partials/uploaded_list.html", context)
     else:
         form = LasUploadForm()
         context = {
--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wellcast_tailwind_temp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_asgi_application()
